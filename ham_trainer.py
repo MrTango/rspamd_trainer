4085bf0c28899de067b52901c6bd2774588eaf97
MrTango <md@derico.de>
2017-01-27 18:19:31 +0100

    excludes directories before getting files

--- ham_trainer.py
+++ ham_trainer.py
@@ -13,30 +13,34 @@ counter = 0
 
 t1 = time.time()
 for dom in included_domains:
-    for item in Path(start_path).rglob(dom + '/*/**/cur/*'):
-        raw_counter += 1
-        item_mtime = item.stat().st_mtime
-        if not (item_mtime < min_time and item_mtime > max_time):
-            continue
-        if not item.is_file():
+    for item in Path(start_path).rglob(dom + '/*/**/cur'):
+        if not item.is_dir():
             continue
         if not ham_exclude_parts.isdisjoint(PurePath(item).parts):
-            # logger.debug("Skip because of excluded parts match: %s" % str(
-            #    item.absolute()))
+            logger.debug(
+                "Skip because of excluded parts match: %s" % str(
+                    item.absolute()))
             continue
-        counter += 1
-        with subprocess.Popen(
-                ["rspamc", "learn_ham", str(item.absolute())],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.STDOUT) as p:
-            try:
-                outs, errs = p.communicate(timeout=15)
-            except subprocess.TimeoutExpired:
-                p.kill()
-                outs, errs = p.communicate()
-            logger.debug(outs)
+        for message_file in item.glob("*.*"):
+            raw_counter += 1
+            msg_mtime = message_file.stat().st_mtime
+            if not message_file.is_file():
+                continue
+            if not (msg_mtime < min_time and msg_mtime > max_time):
+                continue
+            counter += 1
+            with subprocess.Popen(
+                    ["rspamc", "learn_ham", str(message_file.absolute())],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT) as p:
+                try:
+                    outs, errs = p.communicate(timeout=15)
+                except subprocess.TimeoutExpired:
+                    p.kill()
+                    outs, errs = p.communicate()
+                logger.debug(outs)
 
 
 logger.info(
     "Learned ham from user folders with %s out of %s files in %s minutes." % (
-        counter, raw_counter, int((time.time()-t1)) / 60))
+        counter, raw_counter, int((time.time() - t1)) / 60))
